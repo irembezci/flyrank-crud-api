20c893e27083888e9aff4f63a054bcd8fd960cbe
Irem Bezci <irlinya@Irems-MacBook-Pro.local>
2026-07-19 18:33:52 +0300

    Stage 3: implement POST task endpoint

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class TaskCreate(BaseModel):
+    title: str
 
 tasks = [
     {
@@ -46,3 +50,15 @@ def get_task(task_id: int):
         status_code=404,
         detail=f"Task {task_id} not found"
     )
+
+@app.post("/tasks", status_code=201)
+def create_task(task: TaskCreate):
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": task.title,
+        "done": False
+    }
+
+    tasks.append(new_task)
+
+    return new_task
