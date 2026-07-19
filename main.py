e66c73c3f8441e4e6aaafef44c05c35fda1273f8
Irem Bezci <irlinya@Irems-MacBook-Pro.local>
2026-07-19 18:36:57 +0300

    Stage 4: implement PUT and DELETE task endpoints

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 class TaskCreate(BaseModel):
     title: str
 
+class TaskUpdate(BaseModel):
+    title: str
+    done: bool
+
 tasks = [
     {
         "id": 1,
@@ -62,3 +66,28 @@ def create_task(task: TaskCreate):
     tasks.append(new_task)
 
     return new_task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskUpdate):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = updated_task.title
+            task["done"] = updated_task.done
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
